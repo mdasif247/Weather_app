4a784034f4488ed5889cb2c827e3fad74af61f23
asif <asifp106@gmail.com>
2023-07-04 18:36:03 +0530

    Initial Commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "e8c098ca0e281043db6312c59f09be70",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
